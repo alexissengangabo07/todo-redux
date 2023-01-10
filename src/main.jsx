022b914f99197fd0e1100e781c96fdc8b943a5fb
alexissengangabo07 <alexxngabo@gmail.com>
2023-01-10 13:49:03 +0200

    feat: implement redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import reducers from './reducers';
+import App from './App';
+import './index.css';
+
+const store = createStore(
+  combineReducers(reducers),
+  composeWithDevTools(applyMiddleware(thunk))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+);
